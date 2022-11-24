cede80e532a9593889fba5dc13d88c2b4c500eef
ismailhossendev <ismailhossendev@gmail.com>
2022-11-24 16:30:30 +0600

    category route added

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.odx3u2z.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-const appointmentOptions = client.db("drivingPortal").collection("appointmentOptions");
+const categories = client.db("dream_book").collection("categories");
 
 //routes
 app.get('/', (req, res) => {
@@ -25,6 +25,16 @@ app.get('/', (req, res) => {
         message: 'server is running'
     })
 })
+
+app.get('/categories', async (req, res) => {
+    const limit = req.query.limit ? parseInt(req.query.limit) : 0;
+    const result = await categories.find({}).limit(limit).toArray();
+    console.log(result);
+    res.send(result);
+})
+
+
+
 
 
 
